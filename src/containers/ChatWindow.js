30573ebc510fae461cfabcaea0d1e90c283da28a
luis <luis.scholars@gmail.com>
2019-02-16 14:16:54 -0500

    Chat Window message display functionality

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import store from '../store';
 import Header from '../components/Header';
+import Chats from '../components/Chats';
+import './ChatWindow.css';
 
 const ChatWindow = ({ activeUserId }) => {
 	const state = store.getState();
 	const activeUser = state.contacts[activeUserId];
-
+	const activeMsgs = state.messages[activeUserId];
 	return (
 		<div className="ChatWindow">
 			<Header user={activeUser} />
+			<Chats messages={Object.values(activeMsgs)} />
 		</div>
 	);
 };
