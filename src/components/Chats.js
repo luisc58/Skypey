0ea592fd444cf858de6a30639484a2ef817b7fb7
luis <luis.scholars@gmail.com>
2019-02-18 20:22:35 -0500

    Finished messenger functionality

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -6,9 +6,23 @@ const Chat = ({ message }) => {
 	return <span className={`Chat ${is_user_msg ? 'is-user-msg' : ''}`}>{text}</span>;
 };
 class Chats extends Component {
+	constructor(props) {
+		super(props);
+		this.chatsRef = React.createRef();
+	}
+	componentDidMount() {
+		this.scrollToBottom();
+	}
+	componentDidUpdate() {
+		this.scrollToBottom();
+	}
+
+	scrollToBottom = () => {
+		this.chatsRef.current.scrollTop = this.chatsRef.current.scrollHeight;
+	};
 	render() {
 		return (
-			<div className="Chats">
+			<div className="Chats" ref={this.chatsRef}>
 				{this.props.messages.map((message) => <Chat message={message} key={message.number} />)}
 			</div>
 		);
